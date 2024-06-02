85256e9f180941b0e3b5c9500368c6193051c07c
luciorim <88190686+luciorim@users.noreply.github.com>
2024-06-02 19:51:53 +0500

    initial structure

---
+++ src/main/java/com/luciorim/socnetserver/SocnetServerApplication.java
@@ -0,0 +1,13 @@
+package com.luciorim.socnetserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocnetServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocnetServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luciorim/socnetserver/SocnetServerApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.luciorim.socnetserver;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SocnetServerApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
